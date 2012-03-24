a6427232f6e0799ba9bc6d76293f7b2b5ae25de6
HighVoltz82@gmail.com <HighVoltz82@gmail.com@a5cd590f-af6d-4c75-87b2-698d226b09eb>
2012-03-24 01:06:06 +0000

    fixes

---
+++ trunk/Composites/CombatAction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Styx;
+using Styx.Logic.Combat;
+using Styx.Logic.POI;
+using Styx.WoWInternals;
+using Styx.WoWInternals.WoWObjects;
+using TreeSharp;
+using System.Linq;
+
+namespace HighVoltz.Composites
+{
+    public class CombatAction : Action
+    {
+        protected override RunStatus Run(object context)
+        {
+            if (BotPoi.Current != null && BotPoi.Current.Type == PoiType.Harvest)
+            {
+                MoveToPoolAction.MoveToPoolSW.Reset();
+                MoveToPoolAction.MoveToPoolSW.Start();
+            }
+            Utils.EquipWeapon();
+            return RunStatus.Failure;
+        }
+    }
+}
