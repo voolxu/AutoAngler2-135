accb8bf2c9f76519db55232d3d1c4ee496df6e3d
HighVoltz82@gmail.com <HighVoltz82@gmail.com@a5cd590f-af6d-4c75-87b2-698d226b09eb>
2013-04-05 21:40:51 +0000

    Added Nat's hat and Anglers Fishing Raft Provided by WileE91. Thank you

--- trunk/Composites/ApplyLureAction.cs
+++ trunk/Composites/ApplyLureAction.cs
@@ -35,6 +35,13 @@ namespace HighVoltz.Composites
             return RunStatus.Failure;
         }
 
+        readonly uint[] _fishingHatIds = new uint[]
+                                         {
+                                             33820, // Weather-Beaten Fishing Hat
+                                             88710 , // Nat's Hat
+                                         };
+
+
         // does nothing if no lures are in bag
         private bool Applylure()
         {
@@ -45,24 +52,25 @@ namespace HighVoltz.Composites
             if (_me.Inventory.Equipped.MainHand != null &&
                 _me.Inventory.Equipped.MainHand.ItemInfo.WeaponClass != WoWItemWeaponClass.FishingPole)
                 return false;
-            // Weather-Beaten Fishing Hat  
-            WoWItem head = _me.Inventory.GetItemBySlot((uint) WoWEquipSlot.Head);
-            if (head != null && head.Entry == 33820)
-            {
-                AutoAngler.Instance.Log("Appling Weather-Beaten Fishing Hat to fishing pole");
-                Utils.UseItemByID(33820);
-                return true;
-            }
-			
-			//Awesome panda lure
-			WoWItem pandalure = StyxWoW.Me.BagItems.FirstOrDefault(r => r.Entry == 85973);
+
+            //Awesome panda lure
+            WoWItem pandalure = StyxWoW.Me.BagItems.FirstOrDefault(r => r.Entry == 85973);
             if (pandalure != null && !_me.HasAura(125167))
             {
                 AutoAngler.Instance.Log("Appling awesome panda lure");
                 Utils.UseItemByID(85973);
                 return true;
             }
-			
+
+            // Fishing Hats
+            WoWItem head = _me.Inventory.GetItemBySlot((uint)WoWEquipSlot.Head);
+            if (head != null && (head.Entry == 88710 || head.Entry == 33820))
+            {
+                AutoAngler.Instance.Log("Appling Fishing Hat lure to fishing pole");
+                Utils.UseItemByID((int)head.Entry);
+                return true;
+            }
+
             foreach (var kv in Lures)
             {
                 WoWItem lureInBag = Utils.GetIteminBag(kv.Key);
