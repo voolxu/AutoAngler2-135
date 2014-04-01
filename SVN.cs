97ab78a372d3e4bb36111201317fa87788029d6d
HighVoltz82@gmail.com <HighVoltz82@gmail.com@a5cd590f-af6d-4c75-87b2-698d226b09eb>
2014-04-01 00:00:11 +0000

    Added the aura 'Blessing of the Inkgill' to list of water walking buffs.

--- SVN.cs
+++ SVN.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
-// Build Date: 2014/02/25 21:21:15
+// Build Date: 2014/03/13 19:40:12
 // SVN url: https://autoangler2.googlecode.com/svn/trunk
 
 using System;
@@ -15,7 +15,7 @@ namespace HighVoltz.AutoAngler
         {
             get
             {
-                return "113";
+                return "114";
             }
         }
     }
