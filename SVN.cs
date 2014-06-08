78542129d006f2ddf2e3e6e6903a42c8104e67a4
HighVoltz82@gmail.com <HighVoltz82@gmail.com@a5cd590f-af6d-4c75-87b2-698d226b09eb>
2014-06-08 01:28:53 +0000

    Internal changes to the mailing behavior.

--- SVN.cs
+++ SVN.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
-// Build Date: 2014/06/01 23:49:55
+// Build Date: 2014/06/06 23:55:40
 // SVN url: https://autoangler2.googlecode.com/svn/trunk
 
 using System;
@@ -15,7 +15,7 @@ namespace HighVoltz.AutoAngler
         {
             get
             {
-                return "120";
+                return "123";
             }
         }
     }
