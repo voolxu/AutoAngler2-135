be5efdbc50eafb609a7a9bbb36d2249091d6296a
HighVoltz82@gmail.com <HighVoltz82@gmail.com@a5cd590f-af6d-4c75-87b2-698d226b09eb>
2014-06-09 21:52:54 +0000

    The auto-face water algorithm will now exclude underground water areas

--- SVN.cs
+++ SVN.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
-// Build Date: 2014/06/07 21:28:53
+// Build Date: 2014/06/08 17:49:32
 // SVN url: https://autoangler2.googlecode.com/svn/trunk
 
 using System;
@@ -15,7 +15,7 @@ namespace HighVoltz.AutoAngler
         {
             get
             {
-                return "124";
+                return "125";
             }
         }
     }
