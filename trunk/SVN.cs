f19e83578031e733bbf5e5657b99430649d5de67
HighVoltz82@gmail.com <HighVoltz82@gmail.com@a5cd590f-af6d-4c75-87b2-698d226b09eb>
2014-08-31 02:45:33 +0000

    * PathType and FishingSchool list are now set to default values before a new profile is loaded. * Removed unused files. * Removed the auto-updater

--- trunk/SVN.cs
+++ trunk/SVN.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
-// Build Date: 2014/07/03 21:17:40
+// Build Date: 2014/07/03 23:55:49
 // SVN url: https://autoangler2.googlecode.com/svn/trunk
 
 using System;
@@ -15,7 +15,7 @@ namespace HighVoltz.AutoAngler
         {
             get
             {
-                return "129";
+                return "130";
             }
         }
     }
